04c023138104a301fe5ee6de26c5a040f8be8224
khushwantSinghRathore <khushu36@gmail.com>
2021-02-05 17:19:49 +0530

    ux/ui updated

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
+import { Router } from '@angular/router';
+import { SongslistPage } from '../songslist/songslist.page';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home', 
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  songlist(){
+    this.router.navigate(['/songslist']);
+  }
 
 }
--- src/app/songslist/songslist.page.ts
+++ src/app/songslist/songslist.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-songslist',
   templateUrl: './songslist.page.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SongslistPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  back(){
+    this.router.navigate(['/home']);
   }
 
 }
